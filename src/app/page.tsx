b307e997680612f3ab4e5975377393c3a036ceea
Ola Okegbemi <okegbemiolatunde@gmail.com>
2024-03-20 15:45:05 +0100

    added changeset

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="main">
       <div>
-        <h1>New addition</h1>
+        <h1>Changeset newer addition</h1>
         <h2>Hello World</h2>
       </div>
       <Link href="/about">About</Link>
